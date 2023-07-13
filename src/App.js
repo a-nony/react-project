00ec732ef4d69b72c79437b877e7bca41fbffd09
a-nony <usmanmustafa621@gmail.com>
2023-07-13 15:12:11 +0100

    react-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+/*Importing of all the necessary resources into app.js */
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import Button from 'react-bootstrap/Button';
+import vinicius from './assets/vinicius.jpeg'
+import haaland from './assets/haaland.jpeg'
+import mosalah from './assets/mosalah.jpeg'
+import Card from 'react-bootstrap/Card';
 
-function App() {
+// the funtion 'App' to  be rendered in index to js
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='App'>
+         <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home"><h5>React Bootstrap Checkpoint</h5></Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    
+      {/*container tag containing all of the cards */}
+      <container>
+        <div className='card-container' style={{  display: 'grid', gridTemplateColumns: '1fr 1fr 1fr'}}>
+        <Card style={{ width: '18rem', margin: '9px' }}>
+        { /*Importing of the images from the assets folder */}
+      <Card.Img variant="top" src={vinicius} />
+      <Card.Body>
+        <Card.Title>Vinicius jr.</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        {/* <Button variant="primary">Go somewhere</Button> */}
+      </Card.Body>
+    </Card>
 
+    <Card style={{ width: '18rem', margin: '9px' }}>
+    { /*Importing of the images from the assets folder */}
+      <Card.Img variant="top" src={haaland} />
+      <Card.Body>
+        <Card.Title>Erling haaland</Card.Title>
+        <Card.Text>
+        Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        {/* <Button variant="primary">Go somewhere</Button> */}
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem', margin: '9px' }}>
+       { /*Importing of the images from the assets folder */}
+      <Card.Img variant="top" src={mosalah} />
+      <Card.Body>
+        <Card.Title>Mo salah</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        {/* <Button variant="primary">Go somewhere</Button> */}
+      </Card.Body>
+    </Card>
+
+        </div>
+      </container>
+
+     
+      {/* <br />
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <br />
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar> */}
+    </>
+    </div>
+  )
+};
+
+//exporting the 'App function'
 export default App;
+
+
+
+
+// function BasicExample() {
+//   return (
+    
+//   );
+// }
+
+// export default BasicExample;
+
+
+// function ColorSchemesExample() {
+//   return (
+   
+//   );
+// }
+
+// export default ColorSchemesExample;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//importing the app funtion from 'App.js'
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
